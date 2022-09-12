2dd312dae652e452ad2d25a17ba2a1c7414bcf43
Andrea Valente <104693583+avalenteAvo@users.noreply.github.com>
2022-09-12 11:35:28 +0200

    Update es12.js

--- es12.js
+++ es12.js
@@ -1,24 +1,13 @@
-/* Creare un progetto Javascript che dichiari e contestualmente inizializzi la costante MAX a 15;
-che dichiari le variabili a, b e c contemporaneamente; che assegni ad a,b e c rispettivamente i numeri 10, 11 e 12;
-che successivamente incrementi le tre variabili della costante dichiarata in apertura e
-che infine vi sottragga un valore numerico costante a scelta del programmatore (senza dichiarare la costante).*/
+var d = Number(window.prompt("Inserisci il Diametro D: ", "nessuno"));
+let x = Math.PI;
+var r, a,circ;
 
+r = d/2;
+console.log(r);
+a = r*r*x;
+a = parseFloat(a).toFixed(3);
+circ = d * x;
+circ = parseFloat(circ).toFixed(3);
 
-var MAX = 15;
-var a,b,c;
-
-a = 10;
-b= 11;
-c = 12;
-
-a = a + MAX;
-b= b + MAX;
-c = c + MAX;
-
-a = a - 25;
-b= b - 25;
-c = c - 25;
-
-document.write('<h1>' + a + '</h1>');
-document.write('<h1>' + b + '</h1>');
-document.write('<h1>' + c + '</h1>');
+// Versione da principiante
+// document.write("<p> Un cerchio di diametro " + d + " ha area " + a + " e circonferenza " + circ + "</p>");
