c2a58f49dee186046b4e6d91ab24ad8c81db97a8
Andrea Valente <104693583+avalenteAvo@users.noreply.github.com>
2022-10-28 22:56:02 +0200

    Delete es1.js

--- es1.js
+++
@@ -1,8 +0,0 @@
-function alertPost(){
-  alert("addio");
-}
-alert("ciao")
-document.write('<h1> Andrea Valente </h1>');
-document.write('<h2> Nato a Brindisi </h2>');
-document.write('<h2> Il 22 Settembre 1988 </h2>');
-myTimeout = setTimeout(alertPost, 1000);
